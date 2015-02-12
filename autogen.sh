def4e627804270f6a55da3ddafef5e90e39fbaa9
Earlz <earlz@earlz.net>
2015-02-11 21:12:07 -0500

    ignore errors cause fuck gitian

--- autogen.sh
+++ autogen.sh
@@ -5,5 +5,5 @@ cd "$srcdir"
 if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
   export LIBTOOLIZE
-fi
-autoreconf --install --force --warnings=all
+fi  
+autoreconf --install --force
