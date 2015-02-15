57169f4e36ef2da41620af5a47ac4915bdb48e55
Earlz <earlz@earlz.net>
2015-02-14 19:40:02 -0500

    fix clearblocks

--- src/clearblocks.sh
+++ src/clearblocks.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rm -rf ~/.bitcoin/fee_estimates.dat ~/.bitcoin/blocks/ ~/.bitcoin/chainstate/ ~/.bitcoin/debug.log ~/.bitcoin/db.log ~/.bitcoin/database/ ~/.bitcoin/peers.dat
-
+rm -rf ~/.supcoin/fee_estimates.dat ~/.supcoin/blocks/ ~/.supcoin/chainstate/ ~/.supcoin/debug.log ~/.supcoin/db.log ~/.supcoin/database/ ~/.supcoin/peers.dat
